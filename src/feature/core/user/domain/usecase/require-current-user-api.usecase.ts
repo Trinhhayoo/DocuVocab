ed9bca34ccf8958650b68358b402092fdc809e0a
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2026-07-08 14:15:00 +0700

    Update defined return type

--- src/feature/core/user/domain/usecase/require-current-user-api.usecase.ts
+++ src/feature/core/user/domain/usecase/require-current-user-api.usecase.ts
@@ -1,11 +1,11 @@
 // src/feature/core/user/domain/usecase/require-current-user-api.usecase.ts
 
 import { createSupabaseServerClient } from "@/lib/supabase/server";
-import { failure, success } from "@/feature/common/data/result";
+import { failure, Result, success } from "@/feature/common/data/result";
 import UnauthorizedFailure from "../failure/unauthorized.failure";
 import type { User } from "@supabase/supabase-js";
 
-export default async function requireCurrentUserApiUsecase() {
+export default async function requireCurrentUserApiUsecase(): Promise<Result<User>> {
   const supabase = await createSupabaseServerClient();
 
   const {
