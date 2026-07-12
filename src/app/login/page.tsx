65006def7b6810450460e467058692dafbd76c16
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2026-07-12 18:08:11 +0700

    Update origin url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
@@ -5,11 +6,19 @@ export default async function LoginPage() {
     async function signInWithGoogle() {
         "use server";
 
+        const headerList = await headers();
+        const forwardedHost = headerList.get("x-forwarded-host");
+        const forwardedProto = headerList.get("x-forwarded-proto");
+        const origin =
+          forwardedProto && forwardedHost
+            ? `${forwardedProto}://${forwardedHost}`
+            : process.env.NEXT_PUBLIC_APP_URL!;
+
         const supabase = await createSupabaseServerClient();
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+                redirectTo: new URL("/auth/callback", origin).toString(),
             },
         });
 
--- src/lib/supabase/signInWithGoogle.ts
+++ src/lib/supabase/signInWithGoogle.ts
@@ -3,12 +3,12 @@ import { redirect } from "next/navigation";
 import { createSupabaseBrowserClient } from "./client";
 
 export async function signInWithGoogle() {
-
+    const origin = window.location.origin;
     const supabase = await createSupabaseBrowserClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/web/callback`,
+            redirectTo: new URL("/auth/web/callback", origin).toString(),
         },
     });
 
